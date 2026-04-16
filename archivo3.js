57e4f397b5e1672fc7743b97241b5d6f0e58adf6
Lucas Mejia <A01646442@tec.mx>
2026-04-16 08:10:32 -0600

    Add chalk package example

---
+++ archivo3.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hola desde chalk"));
+console.log(chalk.green("Este texto sale en verde"));
+console.log(chalk.bold.red("Este texto sale en rojo y negrita"));
